73e1490df6f14adc19b65d16fcfacbeb4e6dd5e8
Vikas K U <vikasku@thoughtworks.com>
2016-02-22 20:15:47 +0530

    library for word count has created

---
+++ src/WordCount.java
@@ -0,0 +1,18 @@
+
+class WordCount {
+	String text;
+	public WordCount(String text){
+		this.text = text;
+	}
+	public int charCount(){
+		return text.length();
+	};
+	public int lineCount(){
+		return text.split("\n").length-1;
+	};
+	public int wordCount(){
+		String[] empty={};
+		String[] word = text.isEmpty() ? empty :text.trim().replaceAll("\\s+"," ").split(" ");
+		return word.length;
+	}
+}
